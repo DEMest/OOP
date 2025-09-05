9b181ecfa5b971560ddfe603e8057ce856b0afc2
Daniel Smolin <danielsmolin5@gmail.com>
2025-09-05 15:33:52 +0700

    Added docs

--- Task_1_1_1/src/main/java/ru/nsu/smolin/Sample.java
+++ Task_1_1_1/src/main/java/ru/nsu/smolin/Sample.java
@@ -4,29 +4,60 @@ class MaxHeap {
     public int[] arr;
     private int hsize;
 
+    /**
+     * Initializes a new MaxHeap
+     * @param n capacity of new heap
+     */
     public MaxHeap(int n) {
         arr = new int[n];
         hsize = 0;
     }
 
+    /**
+     * function for swapping elements in MaxHeap
+     * @param arr A heap of elements that we want to swap
+     * @param a First Element
+     * @param b Second Element
+     */
     private void swap(int[] arr, int a, int b) {
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
 
+    /**
+     * Determines the parent element index of the
+     * selected element index
+     * @param a Selected element index
+     * @return parent elemnt index
+     */
     private int parentidx(int a) {
         return (a - 1) / 2;
     }
 
+    /**
+     * Returns the right child element index
+     * @param a Root element index
+     * @return child element index
+     */
     private int right(int a) {
         return 2 * a + 2;
     }
 
+    /**
+     * Returns the left child element index
+     * @param a Root element index
+     * @return child element index
+     */
     private int left(int a) {
         return 2 * a + 1;
     }
 
+    /**
+     * Implements the function to insert
+     * an element into a heap
+     * @param a element you want to insert
+     */
     public void insert(int a) {
         int i = hsize;
         arr[i] = a;
@@ -38,6 +69,17 @@ class MaxHeap {
         }
     }
 
+    /**
+     * Ensures the maxHeap property
+     * for a subtree.
+     * It compares a node with its
+     * children, swaps with the
+     * larger child if necessary,
+     * and recursive going down until
+     * the subtree satisfies the heap
+     * property.
+     * @param a Root node
+     */
     private void heapify(int a) {
         int l = left(a);
         int r = right(a);
@@ -54,6 +96,12 @@ class MaxHeap {
         }
     }
 
+    /**
+     * Sorts the elements of the heap in ascending order
+     * swapping root element with last, deacreasing capacity of heap.
+     * the heapify tree without root element to recursive determine
+     * max element (root) in a tree.
+     */
     public void heapsort() {
         int savesize = hsize;
 
@@ -65,6 +113,11 @@ class MaxHeap {
         hsize = savesize;
     }
 
+    /**
+     * getting element from private param of heap
+     * @param i index of getting element
+     * @return element staying on index
+     */
     public int getElem(int i) {
         return arr[i];
     }
@@ -85,6 +138,11 @@ public class Sample {
         }
     }
 
+    /**
+     * checking array for tests
+     * @param arr unsorted array
+     * @return sorted
+     */
     public static int[] check(int[] arr) {
         int cap = arr.length;
         MaxHeap a = new MaxHeap(cap);
