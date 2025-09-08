7a5bd85050c0f9bf3832704e23669551b6512e38
DEMest <DEMest@users.noreply.github.com>
2025-09-08 03:32:22 +0000

    Deploying to gh-pages from @ DEMest/OOP@5cc5d9cbb1592cfe56b26e0712b2301f2e7f2341 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.smolin","l":"Sample"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.smolin","l":"Main"},{"p":"ru.nsu.smolin","l":"MaxHeap"}];updateSearchResults();
