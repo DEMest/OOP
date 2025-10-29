710d5bf9d4bed51950e4da5c85c54fec025b6ade
DEMest <DEMest@users.noreply.github.com>
2025-10-29 13:03:08 +0000

    Deploying to gh-pages from @ DEMest/OOP@51718c9ea26ec80b4b6f3cd33fb87f20ff608e95 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.smolin"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.smolin"},{"l":"ru.nsu.smolin.impl"}];updateSearchResults();
